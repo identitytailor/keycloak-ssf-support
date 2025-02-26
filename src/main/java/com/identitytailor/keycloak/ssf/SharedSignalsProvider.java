52c62d46ba61f6bc117e460df2aa668c6a8a49fb
Thomas Darimont <thomas.darimont@googlemail.com>
2025-02-26 15:33:23 +0000

    Polishing

--- src/main/java/com/identitytailor/keycloak/ssf/SharedSignalsProvider.java
+++ src/main/java/com/identitytailor/keycloak/ssf/SharedSignalsProvider.java
@@ -1,15 +1,14 @@
 package com.identitytailor.keycloak.ssf;
 
-import com.identitytailor.keycloak.ssf.event.processor.SecurityEventProcessingContext;
 import com.identitytailor.keycloak.ssf.event.SecurityEventToken;
-import com.identitytailor.keycloak.ssf.event.types.SecurityEvent;
+import com.identitytailor.keycloak.ssf.event.processor.SecurityEventProcessingContext;
+import com.identitytailor.keycloak.ssf.receiver.delivery.poll.SharedSignalsStreamPoller;
+import com.identitytailor.keycloak.ssf.receiver.delivery.push.SharedSignalsPushEndpoint;
 import com.identitytailor.keycloak.ssf.receiver.management.ReceiverManagementEndpoint;
 import com.identitytailor.keycloak.ssf.receiver.management.ReceiverStreamManager;
 import com.identitytailor.keycloak.ssf.receiver.streamclient.SharedSignalsStreamClient;
 import com.identitytailor.keycloak.ssf.receiver.transmitterclient.TransmitterClient;
 import com.identitytailor.keycloak.ssf.receiver.verification.SecurityEventsVerificationClient;
-import com.identitytailor.keycloak.ssf.receiver.delivery.poll.SharedSignalsStreamPoller;
-import com.identitytailor.keycloak.ssf.receiver.delivery.push.SharedSignalsPushEndpoint;
 import com.identitytailor.keycloak.ssf.storage.SharedSignalsStore;
 import org.keycloak.provider.Provider;
 
